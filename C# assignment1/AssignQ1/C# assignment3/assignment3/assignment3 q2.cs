31f39f5be9f495c64be9ba4ebfbb2736c357ad3e
amatuljabbar18 <115521040+amatuljabbar18@users.noreply.github.com>
2022-11-10 17:44:47 +0000

    Add files via upload

---
+++ C# assignment1/AssignQ1/C# assignment3/assignment3/assignment3 q2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static assignment3.assignment3.EmployeeManagementSystem;
+namespace assignment3
+{
+    class Q2
+    {
+
+
+
+int[] Mystack;
+int top;
+int max;
+public void Stack(int size)
+{
+    Mystack = new int[size];
+    top = -1;
+    max = size;
+}
+public void Push(int size)
+{
+    try
+    {
+        if (top == 5)
+        {
+            Console.WriteLine("The Stack is full");
+        }
+        else
+        {
+            top++;
+            Mystack[top] = size;
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
+public void pop()
+{
+    int Element;
+    if (top == -1)
+    {
+        Console.WriteLine("The stack is underflow");
+    }
+    else
+    {
+        Element = Mystack[top];
+        top--;
+        Console.WriteLine(" The poped elements =" + Element);
+    }
+}
+public void Display()
+{
+    int i;
+    for (i = top; i >= 0; i--)
+    {
+        Console.WriteLine(Mystack[i]);
+        Console.ReadLine();
+    }
+}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
